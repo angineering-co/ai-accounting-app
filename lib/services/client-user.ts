b9af2707cc12286d4d8ffaa5e587334b3459980b
Ang Wang <chihangwang@users.noreply.github.com>
2026-03-04 21:48:39 +0800

    VERCEL_URL instead of NEXT_PUBLIC_VERCEL_URL

--- lib/services/client-user.ts
+++ lib/services/client-user.ts
@@ -47,7 +47,7 @@ async function requireFirmManager() {
 function getInviteRedirectTo() {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    process?.env?.VERCEL_URL ?? // Automatically set by Vercel.
     'http://127.0.0.1:3000'
 
   // Make sure to include `https://` when not localhost.
